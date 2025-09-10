11ca1ba82e0dd784537748540ec9ac6bd2024d5c
Enes Pala <enespalaio7@gmail.com>
2025-09-10 18:51:02 +0300

    mobile gorunum guncellemesi

--- script.js
+++ script.js
@@ -42,13 +42,12 @@ function productCard(p) {
     : 'placeholder.jpg';
 
   return `
-    <div class="col-6 col-md-6 col-lg-4 product-card mb-4">
+    <div class="product-card mb-4" data-category="${p.category}">
       <div class="card h-100 position-relative">
         <img src="${imgUrl}" class="card-img-top" alt="${p.name}">
         <div class="card-body d-flex flex-column">
           <h5 class="card-title">${p.name}</h5>
           <p class="card-text">${(p.description || '').slice(0, 120)}${(p.description || '').length > 120 ? '…' : ''}</p>
-          <a href="urun.html?slug=${encodeURIComponent(p.slug)}" class="stretched-link"></a>
           <span class="btn btn-gradient mt-auto w-100 text-center">Detay</span>
         </div>
       </div>
